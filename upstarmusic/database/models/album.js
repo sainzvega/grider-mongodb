29195c2f6e2355c36121f55ed1a380d9d91d65aa
Humberto A. Sainz <SAINZHA@innovasi.com>
2018-05-29 15:46:11 -0700

    Update package.json to fix broken electron issue. Add functionality to basic queries

--- upstarmusic/database/models/album.js
+++ upstarmusic/database/models/album.js
@@ -1,1 +1,12 @@
-// Todo: create Album Schema
+const Schema = require('mongoose').Schema;
+
+const AlbumSchema = new Schema({
+    title: String,
+    date: Date,
+    copiesSold: Number,
+    numberTracks: Number,
+    image: String,
+    revenue: Number
+});
+
+module.exports = AlbumSchema;
